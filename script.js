fbbfc0a15144ec9cdfef0f27a78a0c4fd8b91d20
NEEDF0sp33d <luisf3525@gmail.com>
2022-12-27 13:46:29 -0500

    minor changes

--- script.js
+++ script.js
@@ -40,21 +40,3 @@ function Game() {
 
 Game()
 
-let i = 500;
-const para = document.createElement('p');
-
-function isPrime(num) {
-  for(let i = 2; i < num; i++) {
-    if(num % i === 0) {
-      return false;
-    }
-  }
-  return true;
-}
-
-
-
-
-// Don't edit the code below here!
-const section = document.querySelector('section');
-section.appendChild(para);
